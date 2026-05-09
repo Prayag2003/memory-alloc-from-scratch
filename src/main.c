5adc83b359a29d9ecfeaf2825c2a6983d27904d9
Prayag Bhatt <prayagbhatt2003@gmail.com>
2026-05-09 21:07:14 +0530

    [feat-free]: add free block array

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 
 #define HEAP_CAPACITY 1280000
 #define HEAP_ALLOCATED_BLOCKS_CAPACITY 4096
+#define HEAP_FREED_BLOCKS_CAPACITY 4096
 
 /* Internal Heap Storage */
 static char heap_buffer[HEAP_CAPACITY] = {0};
@@ -14,11 +15,37 @@ typedef struct
     size_t size;
 } Heap_Block;
 
-Heap_Block heap_allocated_blocks[HEAP_ALLOCATED_BLOCKS_CAPACITY];
-size_t heap_allocated_blocks_size = 0;
+typedef struct
+{
+    size_t count;
+    Heap_Block blocks[HEAP_ALLOCATED_BLOCKS_CAPACITY];
+} Heap_Block_List;
 
-static int
-heap_has_space(size_t n)
+Heap_Block_List heap_allocated_blocks = {0};
+Heap_Block_List heap_freed_blocks = {0};
+
+void chunk_list_dump(const Heap_Block_List *list)
+{
+    printf("Allocated blocks: %zu\n", list->count);
+    for (size_t i = 0; i < list->count; i++)
+    {
+        printf("Block %zu: start=%p, size=%zu\n", i, list->blocks[i].start, list->blocks[i].size);
+    }
+}
+
+int chunk_list_find(const Heap_Block_List *list, void *ptr)
+{
+}
+
+void chunk_list_remove(Heap_Block_List *list, size_t index)
+{
+}
+
+void chunk_list_add(Heap_Block_List *list, void *start, size_t size)
+{
+}
+
+static int heap_has_space(size_t n)
 {
     return heap_size + n <= HEAP_CAPACITY;
 }
@@ -33,31 +60,12 @@ void *heap_alloc(size_t size)
 
     void *ptr = heap_buffer + heap_size;
     heap_size += size;
-
-    const Heap_Block block = {
-        .start = ptr,
-        .size = size,
-    };
-    assert(heap_allocated_blocks_size < HEAP_ALLOCATED_BLOCKS_CAPACITY && "heap_alloc: too many allocated blocks");
-
-    heap_allocated_blocks[heap_allocated_blocks_size++] = block;
-
+    chunk_list_add(&heap_allocated_blocks, ptr, size);
     return ptr;
-}
-
-void heap_dump_allocated_blocks(void)
-{
-    printf("Allocated blocks: %zu\n", heap_allocated_blocks_size);
-    for (size_t i = 0; i < heap_allocated_blocks_size; i++)
-    {
-        printf("Block %zu: start=%p, size=%zu\n", i, heap_allocated_blocks[i].start, heap_allocated_blocks[i].size);
-    }
 }
 
 void heap_free(void *ptr)
 {
-    (void)ptr;
-    assert(0 && "heap_free is not implemented");
 }
 
 void gc_collect(void *root)
@@ -69,9 +77,15 @@ void gc_collect(void *root)
 int main(void)
 {
     for (int i = 0; i < 10; i++)
-        heap_alloc(i);
+    {
+        void *p = heap_alloc(i);
+        if (i & 1)
+        {
+            heap_free(p);
+        }
+    }
 
-    heap_dump_allocated_blocks();
+    // chunk_list_dump();
     // heap_free(root);
 
     return 0;
