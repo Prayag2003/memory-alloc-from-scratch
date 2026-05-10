b8b6461566da75ed6ab77d12deb5b246d219403c
Prayag Bhatt <prayagbhatt2003@gmail.com>
2026-05-10 19:19:45 +0530

    [refactor]: remove unimplemented garbage collection logic

--- src/heap.h
+++ src/heap.h
@@ -10,29 +10,28 @@ typedef struct
 {
     void *start;
     size_t size;
-} Heap_Block;
+} MemBlock;
 
 typedef struct
 {
+    MemBlock blocks[HEAP_BLOCK_LIST_CAPACITY];
     size_t count;
-    Heap_Block blocks[HEAP_BLOCK_LIST_CAPACITY];
-} Heap_Block_List;
+} MemBlockList;
 
 /* ── Global state (defined in main.c) ── */
 extern char heap_buffer[HEAP_CAPACITY];
-extern Heap_Block_List heap_allocated_blocks;
-extern Heap_Block_List heap_freed_blocks;
+extern MemBlockList allocated_blocks;
+extern MemBlockList free_blocks;
 
 /* ── Allocator API ── */
 void *heap_alloc(size_t size);
 void  heap_free(void *ptr);
 void  heap_coalesce_free_blocks(void);
-void  gc_collect(void *root);
 
 /* ── Block list operations ── */
-void chunk_list_add(Heap_Block_List *list, void *start, size_t size);
-int  chunk_list_find(const Heap_Block_List *list, void *ptr);
-void chunk_list_remove(Heap_Block_List *list, size_t index);
-int  chunk_compare(const void *a, const void *b);
+void block_list_add(MemBlockList *list, void *start, size_t size);
+int  block_list_find(const MemBlockList *list, void *ptr);
+void block_list_remove(MemBlockList *list, size_t index);
+int  block_compare(const void *a, const void *b);
 
 #endif /* HEAP_H */
